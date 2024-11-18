0ed1e435be56274bddfc95d7e37b20185adae1ec
Garrett Birkel <gwbirkel@lbl.gov>
2024-11-18 14:00:18 -0800

    First authentic scan type and set of parameters. Adding validation functions.

--- src/scanTypes.ts
+++ src/scanTypes.ts
@@ -13,10 +13,12 @@ export interface ScanParameterType {
   description: string;
   // If a choices field is present, we provide a pulldown isntead of a text field and restrict the choices to this array.
   choices?: string[];
-  // If no default is provided the default is assumed to be null.
+  // If no default is provided the default is assumed to be "".
   default?: string;
   // If a non-blank value is mandatory for this parameter
   required?: boolean;
+  // Validates the input. Any return value other than null is considered an error and displayed as an error message.
+  validator?: (value:string) => null | string;
 }
 
 
@@ -30,70 +32,98 @@ export interface ScanType {
 
 export interface ScanTypes {
   types: ScanType[];
-  parameters: ScanParameterType[];
+  parameters: Map<Guid, ScanParameterType>;
 }
+
+
+// Handy functions used to assist validation of parameter values.
+
+function isAuto(v:string):boolean { return v.trim().toLowerCase() == "auto"; };
+function isInt(v:string):boolean { return !isNaN(parseInt(v, 10)); };
+function isNumber(v:string):boolean { return !isNaN(v as any); };
+function above(a:number, v:string):boolean { const n = parseFloat(v); return !isNaN(n) && (n > a); };
+function atOrAbove(a:number, v:string):boolean { const n = parseFloat(v); return !isNaN(n) && (n >= a); };
+function atOrBelow(a:number, v:string):boolean { const n = parseFloat(v); return !isNaN(n) && (n <= a); };
+function commaList(test:(v:string) => boolean, c:string):boolean { return c.split(",").every(test) };
 
 
 // This may eventually have to be an asynchronous function
 export function getScanTypes(): ScanTypes {
+
+  const parameters: ScanParameterType[] = [
+
+    { id: "incangles" as Guid,
+      name: "incident angles" as ScanParameterName,
+      description: "A list of incident angles to use, between -30 and 30 degrees. For example 0.13, 0.15, 0.17. Or enter \"auto\" to use the auto-incident angle routine.",
+      default: "auto",
+      required: true,
+      validator: (v) => {
+        if (isAuto(v) || commaList(isNumber, v)) { return null; }
+        return "Must be either a comma-separated list of numbers, or \"auto\".";
+      },
+    },
+    { id: "exptime" as Guid,
+      name: "exposure time" as ScanParameterName,
+      description: "The number of exposure seconds needed, or enter \"auto\" if auto exposure is desired.",
+      default: "auto",
+      required: true,
+      validator: (v) => {
+        if (isAuto(v) || above(0, v)) { return null; }
+        return "Must be either a number, or \"auto\".";
+      },
+    },
+    { id: "mspots" as Guid,
+      name: "measurement spots" as ScanParameterName,
+      description: "The number of measurement spots per sample (2 mm apart, centered around center of sample).",
+      default: "1",
+      validator: (v) => {
+        if (isInt(v) && atOrAbove(1, v)) { return null; }
+        return "Must be an integer, 1 or greater.";
+      },
+    },
+    { id: "expmax" as Guid,
+      name: "max exposure time" as ScanParameterName,
+      description: "The upper limit for exposure time in seconds. Can be up to 120.",
+      default: "120",
+      validator: (v) => {
+        if (atOrAbove(1, v) && atOrBelow(120, v)) { return null; }
+        return "Must be a number from 1 to 120.";
+      },
+    },
+    { id: "imgtype" as Guid,
+      name: "image type" as ScanParameterName,
+      description: "The type of image to generate",
+      required: true,
+      choices: ["single", "tiled"],
+      default: "tiled"
+    }
+  ];
+
+  var parameterMap = new Map<Guid, ScanParameterType>();
+  parameters.forEach((p) => parameterMap.set(p.id, p));
+
+  const types: ScanType[] = [
+    {
+      name: "GIWAXS" as ScanTypeName,
+      description: "GIWAXS",
+      parameters: ["incangles" as Guid, "exptime" as Guid, "mspots" as Guid, "expmax" as Guid, "imgtype" as Guid]
+    }
+//      {
+//        name: "GIWAXS with 3-parameter gpCAM" as ScanTypeName,
+//        description: "GIWAXS with gpCAM optimization. Not all samples may be scanned.",
+//        parameters: ["2" as Guid, "5" as Guid, "6" as Guid, "7" as Guid]
+//      },
+//      {
+//        name: "GIWAXS with 6-parameter gpCAM" as ScanTypeName,
+//        description: "GIWAXS with gpCAM optimization. Not all samples may be scanned.",
+//        parameters: ["2" as Guid, "5" as Guid, "6" as Guid, "7" as Guid]
+//      }
+  ];
+
   return {
-    types: [
-      {
-        name: "GIWAXS" as ScanTypeName,
-        description: "GIWAXS with gpCAM optimization",
-        parameters: ["1" as Guid, "2" as Guid, "3" as Guid, "4" as Guid]        
-      },
-      {
-        name: "GIWAXS with 3-parameter gpCAM" as ScanTypeName,
-        description: "GIWAXS with gpCAM optimization. Not all samples may be scanned.",
-        parameters: ["2" as Guid, "5" as Guid, "6" as Guid, "7" as Guid]
-      },
-      {
-        name: "GIWAXS with 6-parameter gpCAM" as ScanTypeName,
-        description: "GIWAXS with gpCAM optimization. Not all samples may be scanned.",
-        parameters: ["2" as Guid, "5" as Guid, "6" as Guid, "7" as Guid]
-      }
-    ],
-    parameters: [
-      { id: "1" as Guid,
-        name: "incident angles" as ScanParameterName,
-        description: "A list of incident angles to use, between -30 and 30 degrees.  For example 0.13, 0.15, 0.17. Or leave blank to use the auto-incident angle routine."
-      },
-      { id: "2" as Guid,
-        name: "exposure time" as ScanParameterName,
-        description: "The number of exposure seconds needed or, if auto exposure is desired, leave blank.",
-      },
-      { id: "3" as Guid,
-        name: "measurement spots" as ScanParameterName,
-        description: "The number of measurement spots per sample (2 mm apart).",
-        default: "2"
-      },
-      { id: "4" as Guid,
-        name: "image type" as ScanParameterName,
-        description: "The type of image to generate",
-        choices: ["single", "tiled"],
-        default: "tiled"
-      },
-      { id: "5" as Guid,
-        name: "component A (%), gpCAM para 1" as ScanParameterName,
-        description: "Weight percentage of parameter 1.",
-        required: true,
-        default: "33"
-      },
-      { id: "6" as Guid,
-        name: "component B (%), gpCAM para 2" as ScanParameterName,
-        description: "Weight percentage of parameter 2.",
-        required: true,
-        default: "33"
-      },
-      { id: "7" as Guid,
-        name: "component C (%), gpCAM para 3" as ScanParameterName,
-        description: "Weight percentage of parameter 3.",
-        required: true,
-        default: "34"
-      }
-    ]
-  }
+    types: types,
+    parameters: parameterMap
+  };
 }
 
 
