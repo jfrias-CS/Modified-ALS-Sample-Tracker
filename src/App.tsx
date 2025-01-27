acae87d778fc269c167f475f412a0d2d89265278
Garrett Birkel <gwbirkel@lbl.gov>
2025-01-27 10:28:37 -0800

    Commentary about BrowserRouter.  If we do have to deploy this multiple places, this note will help.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,19 @@ import './App.css';
 
 const App: React.FC = () => {
 
+  // You may find this annoying, but it's better to know up front:
+  // The contents of "import.meta.env" are set and used at COMPILE TIME.
+  // By the time your application is served in production, as a set of
+  // static files by (probably) nginx, it is far too late to respond to changes.
+
+  // The point being, if you want BrowserRouter's basename to be set to something
+  // specific for your hosting needs, you need to compile it there in advance,
+  // or fetch the value dynamically in the client from a configuration file in
+  // a known place, e.g. config.json, _before_ you instantiate the BrowserRouter.
+
+  // Here's the catch:  config.json needs to be served in a way that is
+  // unaffected by whatever you use as the base path (and thereby basename) for this app.
+
   return (
     <AppConfigurationProvider>
       <BrowserRouter basename={import.meta.env.BASE_URL}>
