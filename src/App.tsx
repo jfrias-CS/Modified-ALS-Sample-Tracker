41c699ec5c2da7004aa79cb7488a60bb94884d99
Garrett Birkel <gwbirkel@lbl.gov>
2025-03-26 18:04:46 -0700

    Adding in a React provider for userId that redirects if the ID is not present, with a redirect banner in case there is some delay.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
 
 import { AppConfigurationProvider } from './appConfigurationProvider.tsx';
+import { SciCatLoginProvider } from './sciCatLoginProvider.tsx';
 import HomePage from './homePage.tsx';
 import ProposalLayout from "./proposal/proposalLayout.tsx";
 import SetTable from "./proposal/setTable.tsx";
@@ -28,18 +29,21 @@ const App: React.FC = () => {
 
   return (
     <AppConfigurationProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/proposal/:proposalId/" element={<ProposalLayout />}>
-            <Route index element={<SetTable />} /> 
-            <Route path="labels" element={<SetLabels />} />
-            <Route path="set/:setId" element={<Set />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <SciCatLoginProvider>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/proposal/:proposalId/" element={<ProposalLayout />}>
+              <Route index element={<SetTable />} /> 
+              <Route path="labels" element={<SetLabels />} />
+              <Route path="set/:setId" element={<Set />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </SciCatLoginProvider>
     </AppConfigurationProvider>
   )
 }
 
 export default App
+
---
+++ src/sciCatLoginProvider.tsx
@@ -0,0 +1,78 @@
+import { createContext, useState, useEffect, useContext, PropsWithChildren } from "react";
+
+import { AppConfigurationContext } from './appConfigurationProvider.tsx';
+import { getUserIdOrRedirect } from './sciCatBasicApi.ts';
+import { LoadingBanner, LoadingState } from './components/loadingBanner.tsx';
+
+// This is a "context provider" React component for ensuring that we have an authenticated SciCat user.
+
+// It uses the AppConfigurationContext to determine where to redirect when login is needed,
+// and if a user is present, makes the userId available to all sub-components in the React tree.
+
+
+enum LoginCheckState { NotChecked, Pending, Succeeded, Failed };
+
+// The structure we are providing to components in the hierarchy below the provider
+interface SciCatLoginInterface {
+  userId: string | null;
+}
+
+const SciCatLoginContext = createContext<SciCatLoginInterface>({
+                    userId: null
+                  });
+
+const SciCatLoginProvider: React.FC<PropsWithChildren> = (props) => {
+
+  const appConfig = useContext(AppConfigurationContext);
+
+  const [loginState, setLoginState] = useState<LoginCheckState>(LoginCheckState.NotChecked);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [loadingBannerState, setLoadingBannerState] = useState<LoadingState>(LoadingState.Loading);
+  const [loadingMessage, setLoadingMessage] = useState("");
+
+
+  useEffect(() => {
+
+    if (loginState == LoginCheckState.Failed) {
+      setLoadingBannerState(LoadingState.Failure);
+      setLoadingMessage('No user is logged in. Redirecting to SciCat login page...');
+      return;
+    }
+
+    if (loginState == LoginCheckState.Succeeded) {
+      setLoadingBannerState(LoadingState.Success);
+      return;
+    }
+
+    if (loginState == LoginCheckState.Pending) {
+      const id = getUserIdOrRedirect();
+      setUserId(id);
+      if (id === null) {
+        setLoginState(LoginCheckState.Failed);
+      } else {
+        setLoginState(LoginCheckState.Succeeded);
+      }
+    }
+  }, [loginState]);
+
+
+  useEffect(() => {
+    setLoginState(LoginCheckState.Pending);
+  }, []);
+
+  // If we're in any loading state other than success,
+  // display a loading banner instead of the content.
+  if (loginState != LoginCheckState.Succeeded) {
+    return (<LoadingBanner state={loadingBannerState} message={loadingMessage}></LoadingBanner>)
+  }
+
+  return (
+    <SciCatLoginContext.Provider value={{
+      userId: userId
+    }}>
+    {props.children}
+    </SciCatLoginContext.Provider>
+  )
+}
+
+export { SciCatLoginContext, SciCatLoginProvider, LoginCheckState }
