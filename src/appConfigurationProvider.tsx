5d372d36637d79540264838637657bba743099fd
Garrett Birkel <gwbirkel@lbl.gov>
2025-03-26 18:11:08 -0700

    Bringing the visual logger more fully into service for the time being.

--- src/appConfigurationProvider.tsx
+++ src/appConfigurationProvider.tsx
@@ -57,14 +57,14 @@ const AppConfigurationProvider: React.FC<PropsWithChildren> = (props) => {
 
 
   function logger(...args: any[]) {
-//    if (appConfigurationsObject.debugLoggingingEnabled) {
-    const loggingDiv = document.createElement("div");
-    loggingDiv.innerText = [ ...args].join(" ");
-    const c = document.getElementsByTagName("body");
-    c.item(0)?.appendChild(loggingDiv);
+    if (appConfigurationsObject.debugLoggingingEnabled) {
+      const loggingDiv = document.createElement("div");
+      loggingDiv.innerText = [ ...args].join(" ");
+      const c = document.getElementsByTagName("body");
+      c.item(0)?.appendChild(loggingDiv);
 
-//      console.log(...args);
-//    }
+      console.log(...args);
+    }
   }
 
 
